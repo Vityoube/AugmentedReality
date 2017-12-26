53a15d218bf5072c9c21d97836fe8041c595a11c
vkalashnykov <vityoube@gmail.com>
2017-12-26 16:36:08 +0200

    Added project

---
+++ app/src/main/java/com/vkalashnykov/augmentedreality/MainActivity.java
@@ -0,0 +1,39 @@
+package com.vkalashnykov.augmentedreality;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String OpenCV="OpenCV";
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+        System.loadLibrary("opencv_java3");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+        if (!OpenCVLoader.initDebug()){
+            Log.d(OpenCV,"OpenCV Library is not loaded");
+        } else {
+            Log.d(OpenCV,"OpenCV library is loaded");
+        }
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
